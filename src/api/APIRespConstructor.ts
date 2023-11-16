9c345dd0943763d5c35048ced490873054d673c3
r <nobody@localhost>
2023-11-16 23:08:00 +0100

    save oopsie

--- src/api/APIRespConstructor.ts
+++ src/api/APIRespConstructor.ts
@@ -1,5 +1,29 @@
+import { APIResponse } from "./APIResponse";
 
+/**
+ * Constructs a response.
+ * @param code The code to use.
+ * @param message The message to send.
+ */
+function create<T>(success: boolean, code: number, message: string, data?: T): APIResponse<T> {
+    return {
+        code,
+        message,
+        success,
+        data
+    }
+}
 
-export const APIResponder = {
+/**
+ * Constructs a failure response.
+ * @param code The code to use.
+ * @param message The message to send.
+ */
+function fail<T>(code: number, message: string, data?: T): APIResponse<T> {
+    return create(false, code, message, data);
+}
 
+export const APIRespConstructor = {
+    fail,
+    create
 }
