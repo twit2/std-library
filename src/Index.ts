1ce64c26282671a84efb2d9636ada8e2cc06ca6a
r <nobody@localhost>
2023-11-16 20:14:18 +0100

    add msg queue module

--- src/Index.ts
+++ src/Index.ts
@@ -1,8 +1,21 @@
 import { Limits } from './Limits';
+import { MsgQueueProvider } from './comm/MsgQueueProvider';
+import { RabbitMQQueueProvider } from './comm/providers/RabbitMqProvider';
 import { User } from './uam/User';
 import { generateId } from './util/IdGen';
 
+/**
+ * Message queue module.
+ */
+const MsgQueue = {
+    MsgQueueProvider,
+    providers: {
+        RabbitMQQueueProvider
+    }
+}
+
 export {
+    MsgQueue,
     User,
     Limits,
     generateId
