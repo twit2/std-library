27a38003ad9d6e37ece1f37f3869bc6b7479f817
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-11-20 00:24:41 +0100

    add user profile limits

--- src/Limits.ts
+++ src/Limits.ts
@@ -4,11 +4,21 @@
 export const Limits = {
     /** General limits */
     general: {
-        id: { min: 13, max: 20 }
+        id: { min: 13, max: 20 },
+
+        /**
+         * Generic hard limit. Used for non specific fields. Needed to prevent data overrun.
+         */
+        hard: { min: 2, max: 128 },
     },
     /** Users and authentication limits */
     uam: {
         password: { min: 2, max: 64 },
         username: { min: 2, max: 32 }
+    },
+    /** User profile limits */
+    userProfile: {
+        displayName: { min: 0, max: 64 },
+        biography: { min: 0, max: 256 }
     }
 }
