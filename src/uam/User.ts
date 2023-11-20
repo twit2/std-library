ca7c96f623be58512a5a64ac7d96d846ab37f5ce
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-11-20 01:31:30 +0100

    update user profile type

--- src/uam/User.ts
+++ src/uam/User.ts
@@ -3,11 +3,10 @@
  */
 export interface UserProfile {
     id: string;
-    displayName: string;
+    displayName?: string;
     username: string;
-    email: string;
-    avatarURL: string;
-    biography: string;
+    avatarURL?: string;
+    biography?: string;
     dateJoined: Date;
     following: string[];
     followers: string[];
