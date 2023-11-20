5b3039202c5dfa9b6350592212ecc7cceb95b198
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-11-20 01:39:22 +0100

    improve the middleware

--- src/middleware/ErrorHandlingMiddleware.ts
+++ src/middleware/ErrorHandlingMiddleware.ts
@@ -4,7 +4,7 @@ import { APIRespConstructor, APIResponseCodes } from "../Index";
 function handle(err: Error, req: Request, res: Response, next: NextFunction) {
     res.contentType('json');
     res.statusCode = 500;
-    res.end(JSON.stringify(APIRespConstructor.fromCode(APIResponseCodes.GENERIC)));
+    res.end(JSON.stringify(APIRespConstructor.fail(APIResponseCodes.GENERIC, ((err as Error).message) || "<no information>")));
 
     // Print the error
     console.error(`Server error`);
