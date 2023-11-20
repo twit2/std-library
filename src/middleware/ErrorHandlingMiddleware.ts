faa1056d083fba06ec44ab80cf0a1eff29a4598e
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-11-20 01:36:18 +0100

    i forgot

--- src/middleware/ErrorHandlingMiddleware.ts
+++ src/middleware/ErrorHandlingMiddleware.ts
@@ -4,7 +4,7 @@ import { APIRespConstructor, APIResponseCodes } from "../Index";
 function handle(err: Error, req: Request, res: Response, next: NextFunction) {
     res.contentType('json');
     res.statusCode = 500;
-    res.end(JSON.stringify(APIRespConstructor.fromCode(APIResponseCodes.SERVER_ERROR)));
+    res.end(JSON.stringify(APIRespConstructor.fromCode(APIResponseCodes.GENERIC)));
 
     // Print the error
     console.error(`Server error`);
