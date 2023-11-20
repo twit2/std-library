3f76677cd791ceab1f522ac1fc8ead6b3f87dc94
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-11-20 01:36:05 +0100

    Add generic error handler

---
+++ src/middleware/ErrorHandlingMiddleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { APIRespConstructor, APIResponseCodes } from "../Index";
+
+function handle(err: Error, req: Request, res: Response, next: NextFunction) {
+    res.contentType('json');
+    res.statusCode = 500;
+    res.end(JSON.stringify(APIRespConstructor.fromCode(APIResponseCodes.SERVER_ERROR)));
+
+    // Print the error
+    console.error(`Server error`);
+    console.error(err);
+}
+
+export const ErrorHandlingMiddleware = {
+    handle
+}
