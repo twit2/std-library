d95d3604518a69b85307cf26f85b64b6c1d3044e
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-11-24 04:19:39 -0800

    remove ack for development

--- src/comm/providers/RabbitMqProvider.ts
+++ src/comm/providers/RabbitMqProvider.ts
@@ -74,10 +74,10 @@ export class RabbitMQQueueProvider extends MsgQueueProvider {
             if(!msg)
                 return;
 
-            chObj.ch.ack(msg);
+            // TODO NOAck chObj.ch.ack(msg);
             let msgObj = JSON.parse(msg.content.toString()) as QueueMessage<T>;
             cb(msgObj);
-        });
+        }, { noAck: true });
     }
 
     /**
