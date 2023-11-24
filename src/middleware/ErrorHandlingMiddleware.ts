e8f027e28401bceaec59dfceb9467564cc24118c
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-11-25 00:58:04 +0100

    improve error handler

--- src/middleware/ErrorHandlingMiddleware.ts
+++ src/middleware/ErrorHandlingMiddleware.ts
@@ -1,10 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { APIRespConstructor, APIResponseCodes } from "../Index";
+import { APIError, APIRespConstructor, APIResponseCodes } from "../Index";
 
 function handle(err: Error, req: Request, res: Response, next: NextFunction) {
     res.contentType('json');
-    res.statusCode = 500;
-    res.end(JSON.stringify(APIRespConstructor.fail(APIResponseCodes.GENERIC, ((err as Error).message) || "<no information>")));
+
+    if(err instanceof APIError) {
+        res.statusCode = err.status;
+        res.end(JSON.stringify(err.hdr));
+    } else {
+        res.statusCode = 500;
+        res.end(JSON.stringify(APIRespConstructor.fail(APIResponseCodes.GENERIC, ((err as Error).message) || "<no information>")));
+    }
 
     // Print the error
     console.error(`Server error`);
